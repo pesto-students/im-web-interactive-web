17f7fb71a91470f7ca3cf493a9a14a775ad171e9
Rohit Ukirde <ukirderohit@gmail.com>
2021-07-08 22:33:41 +0530

    Integrated Label, Image, Filmcard Component to test in im-ott App

--- packages/im-ott/src/App.js
+++ packages/im-ott/src/App.js
@@ -2,9 +2,28 @@ import "antd/dist/antd.css";
 import "./App.css";
 // Components
 import Button from "imcomponents/atoms/button";
+import { Label } from "imcomponents/atoms/typography";
+import Image from "imcomponents/atoms/image";
+import FilmCard from "imcomponents/molecules/FilmCard";
 
 function App() {
-  return <Button label={"My Button"} />;
+  return (
+    <>
+      <div>
+        <Button label={"My Button"} />
+      </div>
+      <div>
+        <Label>Hi how are you</Label>
+      </div>
+      <>
+        <Image
+          width={200}
+          src="https://homepages.cae.wisc.edu/~ece533/images/airplane.png"
+        />
+      </>
+      <FilmCard className="pl-2" title={"Jab We Met"} year={"2020"} />
+    </>
+  );
 }
 
 export default App;
