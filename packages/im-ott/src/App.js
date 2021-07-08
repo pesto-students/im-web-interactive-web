159689028c9ee33bf53920bed1f723c068f7231e
pranay2401 <pranay.bajaj92@gmail.com>
2021-07-09 01:37:55 +0530

    testing with im-ott

--- packages/im-ott/src/App.js
+++ packages/im-ott/src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
 // Components
+import Button from "imcomponents/atoms/button";
 import Input from "imcomponents/atoms/input";
 import { INPUT_SIZES } from "imcomponents/atoms/input";
 
 function App() {
-  return <Input size={INPUT_SIZES.LARGE} placeholder={"My Input"} />;
+  return (
+    <>
+      <div>
+        <Button label={"My Button"} />
+      </div>
+      <div>
+        <Input placeholder={"My Input"} size={INPUT_SIZES.LARGE} />;
+      </div>
+    </>
+  )
 }
 
 export default App;
