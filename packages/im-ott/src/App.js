75d58a57197a956e710f29231aee03e7737193fe
Rohit Ukirde <ukirderohit@gmail.com>
2021-07-10 13:37:07 +0530

    Squashed commit of the following:
    commit dd06f5f09d85cb7e563fc67eb454053cdb180178
    Merge: bae7c7c f7415bc
    Author: Rohit Ukirde <ukirderohit@users.noreply.github.com>
    Date:   Sat Jul 10 01:26:53 2021 +0530
    
        Merge pull request #4 from pesto-students/feature/filmPage
    
        Feature/film page
    
    commit f7415bc20d7fe97b37ba27975e5dfcdd9c111fd0
    Author: sramineni-suki <sramineni@suki.ai>
    Date:   Sat Jul 10 01:22:04 2021 +0530
    
        filmPage
    
    commit f58ddf1fbc6406719a21c106d056b239ce5957a3
    Author: sramineni-suki <sramineni@suki.ai>
    Date:   Sat Jul 10 00:45:24 2021 +0530
    
        watchList

--- packages/im-ott/src/App.js
+++ packages/im-ott/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // Components
 import AppSkeleton from "./organisms/appSkeleton";
 import LandingPage from "./pages/landingPage";
+import WatchList from "./pages/watchList/WatchList";
+import FilmDetails from "./pages/filmDetails";
 
 // Styles
 import "antd/dist/antd.css";
@@ -14,10 +16,12 @@ function App() {
       <AppSkeleton>
         <Switch>
           <Route exact path="/" component={LandingPage} />
+          <Route exact path="/watchlist" component={WatchList} />
+          <Route exact path="/film/:filmId" component={FilmDetails} />
         </Switch>
       </AppSkeleton>
     </Router>
   );
- }
+}
 
- export default App;
+export default App;
