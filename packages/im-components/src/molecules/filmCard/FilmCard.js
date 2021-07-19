f2faa25cfab2be2573310f838c89684c9f949549
Rohit Ukirde <ukirderohit@gmail.com>
2021-07-19 16:28:56 +0530

    Removed Slider - Made changes to filmcard

--- packages/im-components/src/molecules/filmCard/FilmCard.js
+++ packages/im-components/src/molecules/filmCard/FilmCard.js
@@ -1,14 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
-import cx from "classnames";
 
 // Lodash
 import _times from "lodash/times";
 
 // Component
-import Image from "imcomponents/atoms/image";
 import Tag from "imcomponents/atoms/tag";
-import { Title } from "imcomponents/atoms/typography";
 
 // Icon
 import { StarTwoTone } from "imcomponents/atoms/icon";
@@ -19,28 +16,23 @@ import styles from "./filmcard.module.scss";
 const FilmCard = (props) => {
   const { className, title, imgSrc, genre, rating, isFeatured, ...restProps } =
     props;
-  const filmCardClassName = cx(styles.filmcard, styles.imgGradient, className);
-  const cardWidth = isFeatured ? 370 : 300;
-  const cardHeight = isFeatured ? 370 : 300;
-  const detailsStyle = isFeatured
-    ? styles.detailsFeatured
-    : styles.detailsNotFeatured;
+  const imgStyle = isFeatured ? styles.filmCardImgFeatured : styles.filmCardImg;
 
   return (
-    <div className={filmCardClassName} {...restProps}>
-      <Image src={imgSrc} width={cardWidth} height={cardHeight} />
-      <div className={cx(styles.details, detailsStyle)}>
-        <div>
-          <Tag className={styles.tag} color={"#1d1d1d"}>
-            <span>{genre}</span>
-          </Tag>
+    <div className={styles.filmcard} {...restProps}>
+      <img className={imgStyle} src={imgSrc} alt={title} />
+      <div className={styles.relative}>
+        <div className={styles.details}>
+          <div>
+            <Tag className={styles.tag} color={"#1d1d1d"}>
+              <span>{genre}</span>
+            </Tag>
+          </div>
+          {_times(rating, () => (
+            <StarTwoTone twoToneColor="#fff" />
+          ))}
+          <p className={styles.title}>{title}</p>
         </div>
-        {_times(rating, () => (
-          <StarTwoTone twoToneColor="#fff" />
-        ))}
-        <Title level={3} className={styles.title}>
-          {title}
-        </Title>
       </div>
     </div>
   );
