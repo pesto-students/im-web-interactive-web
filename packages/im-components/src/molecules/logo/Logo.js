d2deec6673325832477bb0336cb666a1fb02732d
pranay2401 <pranay.bajaj92@gmail.com>
2021-07-27 01:01:16 +0530

    Added Logo component

---
+++ packages/im-components/src/molecules/logo/Logo.js
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+
+// Components
+import { VideoCameraFilled } from "imcomponents/atoms/icon";
+
+// Styles
+import styles from "./logo.module.scss";
+import { EMPTY_STRING } from "../../../../im-base/src/constants/base.constants";
+
+const Logo = ( props ) => {
+  const { text, children } = props
+  return (
+    <>
+      <Link to={"/"} className={styles.logo} >
+        <VideoCameraFilled className={styles.image} style={{ fontSize:"250%" }} />
+        <div className={styles.text} >{text}</div>
+      </Link>
+    </>
+  );
+}
+
+Logo.propTypes = {
+  text: PropTypes.string
+};
+
+Logo.defaultProps = {
+  text: EMPTY_STRING
+};
+
+export default Logo;
