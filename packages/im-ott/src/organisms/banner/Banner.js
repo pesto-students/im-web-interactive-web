a30c78a7fde60d0eb20908f02a6233c31a22b615
pranay2401 <pranay.bajaj92@gmail.com>
2021-08-14 03:01:18 +0530

    Removed commented code

--- packages/im-ott/src/organisms/banner/Banner.js
+++ packages/im-ott/src/organisms/banner/Banner.js
@@ -3,7 +3,6 @@ import React from "react";
 // Components
 import { Carousel } from "antd";
 import Image from "imcomponents/atoms/image";
-import { Title, Label } from "imcomponents/atoms/typography";
 
 // Styles
 import styles from "./banner.module.scss";
@@ -12,10 +11,6 @@ const Banner = () => {
   return (
     <Carousel autoplay>
       <div>
-        {/* <div className={styles.bannerOverlay}>
-          <Title level={3}>Intersteller</Title>
-          <Label>n Earth's future, a global crop blight and second Dust Bowl are slowly rendering the planet uninhabitable. Professor Brand (Michael Caine), a brilliant NASA physicist, is working on plans to save mankind by transporting Earth's population to a new home via a wormhole. But first, Brand must send former NASA pilot Cooper (Matthew McConaughey) and a team of researchers through the wormhole and across the galaxy to find out which of three planets could be mankind's new home.</Label>
-        </div> */}
         <Image
           src="https://free4kwallpapers.com/uploads/originals/2020/11/14/interstellar-space-wallpaper.jpg"
           height={500}
