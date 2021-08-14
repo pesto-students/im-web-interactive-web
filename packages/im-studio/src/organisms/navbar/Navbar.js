8ac50015c3df4ecbaeacf19ccdf3d070e3d80586
pranay2401 <pranay.bajaj92@gmail.com>
2021-08-14 16:47:25 +0530

    Updated navBar with icons

--- packages/im-studio/src/organisms/navbar/Navbar.js
+++ packages/im-studio/src/organisms/navbar/Navbar.js
@@ -1,10 +1,17 @@
-import React, { useContext } from "react";
-import { Link, useHistory } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { isMobile, BrowserView, MobileView } from "imcomponents/atoms/device";
 
 // Components
 import { Menu } from "antd";
 import Logo from "imcomponents/molecules/logo";
 import { UserContext } from "imbase/providers/UserProvider";
+import {
+  CloudUploadOutlined,
+  LogoutOutlined,
+  HomeOutlined,
+  HomeFilled,
+} from "imcomponents/atoms/icon";
 
 // Service
 import { logOut } from "imbase/services/firebase";
@@ -15,26 +22,68 @@ import styles from "./navbar.module.scss";
 function Navbar() {
   const history = useHistory();
   const { setuser } = useContext(UserContext);
+  const [selectedMenu, setSelectedMenu] = useState("home");
+
+  const logoText = isMobile ? "Studio" : "iFlix Studio";
+  const HomeIcon = selectedMenu === "home" ? <HomeFilled /> : <HomeOutlined />;
 
   const handleLogout = () => {
     logOut();
     setuser(null);
     history.push("/login");
   };
+
+  const handleClick = (event) => {
+    setSelectedMenu(event.key);
+    if (event.key === "upload") {
+      history.push("/upload");
+    }
+    if (event.key === "home") {
+      history.push("/");
+    }
+  };
+
   return (
-    <div>
-      <Logo text={"iFlix Studio"} />
-      <Menu theme="dark" mode="horizontal" className={styles.floatRight}>
-        <Menu.Item key={"home"}>
-          <Link to={"/"}>Home</Link>
-        </Menu.Item>
-        <Menu.Item key={"upload"}>
-          <Link to={"/upload"}>Upload</Link>
-        </Menu.Item>
-        <Menu.Item key={"logout"}>
-          <div onClick={() => handleLogout()}>Logout</div>
-        </Menu.Item>
-      </Menu>
+    <div className={styles.container}>
+      <Logo text={logoText} />
+      <BrowserView className={styles.browserView}>
+        <Menu
+          mode="horizontal"
+          className={styles.footerMenu}
+          onClick={handleClick}
+        >
+          <Menu.Item
+            className={styles.menuItem}
+            key="home"
+            icon={HomeIcon}
+          ></Menu.Item>
+          <Menu.Item
+            className={styles.menuItem}
+            key="upload"
+            icon={<CloudUploadOutlined />}
+          ></Menu.Item>
+          <Menu.Item
+            key={"logout"}
+            className={styles.menuItem}
+            icon={<LogoutOutlined />}
+            onClick={handleLogout}
+          ></Menu.Item>
+        </Menu>
+      </BrowserView>
+      <MobileView>
+        <Menu
+          mode="horizontal"
+          className={styles.footerMenu}
+          onClick={handleClick}
+        >
+          <Menu.Item
+            key={"logout"}
+            className={styles.menuItem}
+            icon={<LogoutOutlined />}
+            onClick={handleLogout}
+          ></Menu.Item>
+        </Menu>
+      </MobileView>
     </div>
   );
 }
