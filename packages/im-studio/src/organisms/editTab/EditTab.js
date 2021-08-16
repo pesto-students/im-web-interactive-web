a1bf04e14bc853cbd1b99ace4c95615154d12287
pranay2401 <pranay.bajaj92@gmail.com>
2021-08-17 02:35:32 +0530

    Added image upload changes

--- packages/im-studio/src/organisms/editTab/EditTab.js
+++ packages/im-studio/src/organisms/editTab/EditTab.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import Axios from "axios";
+import { Image } from "cloudinary-react";
 
 // Components
 import Button from "imcomponents/atoms/button";
@@ -9,11 +11,13 @@ import Form from "imcomponents/atoms/form";
 import Input from "imcomponents/atoms/input";
 import TextArea from "imcomponents/atoms/textArea";
 
+import _isEmpty from "lodash/isEmpty";
+
 // Sentry
 import * as Sentry from "@sentry/react";
 
 // Constants
-import { EMPTY_OBJECT } from "imbase/constants/base.constants";
+import { EMPTY_OBJECT, EMPTY_STRING } from "imbase/constants/base.constants";
 
 // Redux Actions
 import { updateMovieByID } from "../../redux/movies/actions";
@@ -25,6 +29,13 @@ const EditTab = (props) => {
   const dispatch = useDispatch();
   const { tabdata, history } = props;
   const { id, title, description, url, genre } = tabdata;
+
+  const [selectedBackgroundId, setSelectedBackgroundId] =
+    useState(EMPTY_STRING);
+  const [selectedThumbnailId, setSelectedThumbnailId] = useState(EMPTY_STRING);
+
+  const cloudName = "iflix-cloud";
+  const key = "yqkzec2f";
 
   const formItemLayout = {
     labelCol: { span: 4 },
@@ -63,6 +74,42 @@ const EditTab = (props) => {
 
   const handleSubmit = () => {
     form.submit();
+  };
+
+  const handleThumbnailChange = (event) => {
+    const image = event.target.files[0];
+    if (image) {
+      const formData = new FormData();
+      formData.append("file", image);
+      formData.append("upload_preset", key);
+
+      Axios.post(
+        `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
+        formData
+      ).then((response) => {
+        console.log(response);
+        const publicId = response && response.data && response.data.public_id;
+        setSelectedThumbnailId(publicId);
+      });
+    }
+  };
+
+  const handleBackgroundChange = (event) => {
+    const image = event.target.files[0];
+    if (image) {
+      const formData = new FormData();
+      formData.append("file", image);
+      formData.append("upload_preset", key);
+
+      Axios.post(
+        `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
+        formData
+      ).then((response) => {
+        console.log(response);
+        const publicId = response && response.data && response.data.public_id;
+        setSelectedBackgroundId(publicId);
+      });
+    }
   };
 
   return (
@@ -106,6 +153,38 @@ const EditTab = (props) => {
         >
           <Input placeholder="Enter genre" />
         </Form.Item>
+        <Form.Item label={"Thumbnail Image"} name={"movie_thumbmail"}>
+          <Input
+            className={styles.chooseImageInput}
+            type={"file"}
+            id={"uploadedThumbnail"}
+            onChange={handleThumbnailChange}
+          />
+          {!_isEmpty(selectedThumbnailId) && (
+            <Image
+              className={styles.uploadImagePreview}
+              cloudName={cloudName}
+              publicId={selectedThumbnailId}
+            ></Image>
+          )}
+        </Form.Item>
+
+        <Form.Item label={"Background Image"} name={"movie_background"}>
+          <Input
+            className={styles.chooseImageInput}
+            type={"file"}
+            id={"uploadedBackground"}
+            onChange={handleBackgroundChange}
+          />
+          {!_isEmpty(selectedBackgroundId) && (
+            <Image
+              className={styles.uploadImagePreview}
+              cloudName={cloudName}
+              publicId={selectedBackgroundId}
+            ></Image>
+          )}
+        </Form.Item>
+
         <Form.Item {...buttonItemLayout}>
           <Link to="/dashboard">
             <Button
