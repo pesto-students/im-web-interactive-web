5bc9f1c4532c023870fcd63cb2ea5eaec352dccf
pranay2401 <pranay.bajaj92@gmail.com>
2021-08-20 11:16:17 +0530

    more Youtube url formats supported

--- packages/im-studio/src/pages/upload/helper/upload.general.js
+++ packages/im-studio/src/pages/upload/helper/upload.general.js
@@ -1,3 +1,4 @@
+
 // Constants
 import { EMPTY_STRING } from "imbase/constants/base.constants";
 
@@ -7,28 +8,8 @@ import _isEmpty from "lodash/isEmpty";
 // Services
 import youtubeService from "../../../services/youtubeService";
 
-const getVideoId = (url) => {    
-    if (!youtubeService.matchYoutubeUrl(url)) {
-        return EMPTY_STRING;
-    }
-
-    let videoId = url.split('v=')[1];
-
-    if(_isEmpty(videoId)) {
-        return EMPTY_STRING;
-    }
-
-    const ampersandPosition = videoId.indexOf('&');
-
-    if(ampersandPosition !== -1) {
-        videoId = videoId.substring(0, ampersandPosition);
-    }
-
-    return videoId;
-}
-
 const isUploadDisabled = (videoData) => {
    return _isEmpty(videoData)
 }
 
-export { getVideoId, isUploadDisabled }
+export { isUploadDisabled }
